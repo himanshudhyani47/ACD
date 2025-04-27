ecb92da41198d45560ba9031e095c42a6e4c8854
himanshudhyani47 <himanshu26.dhyani@gmail.com>
2025-04-27 10:03:51 +0530

    develop: added flyway clean feature

--- Flyway/src/main/java/com/services/flyway/FlywayApplication.java
+++ Flyway/src/main/java/com/services/flyway/FlywayApplication.java
@@ -1,5 +1,8 @@
 package com.services.flyway;
 
+import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -9,6 +12,10 @@ public class FlywayApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(FlywayApplication.class, args);
+        Flyway flyway = ctx.getBean(Flyway.class);
+        if(!ctx.getEnvironment().getProperty("spring.flyway.cleanDisabled", Boolean.class, false)) {
+            flyway.clean();
+        }
         SpringApplication.exit(ctx);
     }
 
